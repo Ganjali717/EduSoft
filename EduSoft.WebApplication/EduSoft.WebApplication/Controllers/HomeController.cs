df3c177ffacb387c026a4bc0f980cacc3dc393d2
Ganjali717 <ganjali.imanov@gmail.com>
2023-02-21 16:26:36 +0400

    feature/HM-1-delete-default-classes

--- EduSoft.WebApplication/EduSoft.WebApplication/Controllers/HomeController.cs
+++ EduSoft.WebApplication/EduSoft.WebApplication/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using EduSoft.WebApplication.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace EduSoft.WebApplication.Controllers
@@ -17,16 +16,6 @@ namespace EduSoft.WebApplication.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
--- EduSoft.WebApplication/EduSoft.WebApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EduSoft.WebApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
