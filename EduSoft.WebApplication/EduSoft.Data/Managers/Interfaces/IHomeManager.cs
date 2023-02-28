99478a24c68902d43a4118501c360b0d219d7b58
Ganjali717 <ganjali.imanov@gmail.com>
2023-02-28 15:27:03 +0400

    features/add-home-manager-getalltutorials

--- EduSoft.WebApplication/EduSoft.Data/Managers/Interfaces/IHomeManager.cs
+++ EduSoft.WebApplication/EduSoft.Data/Managers/Interfaces/IHomeManager.cs
@@ -1,6 +1,9 @@
-﻿namespace EduSoft.Data.Managers.Interfaces;
+﻿using EduSoft.Entities;
+using EduSoft.Entities.Tutorials;
+
+namespace EduSoft.Data.Managers.Interfaces;
 
 public interface IHomeManager
 {
-    
+    public ManagerResult<Tutorial> GetTutorial();
 }
